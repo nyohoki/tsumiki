c540e9cf972f2b0af2cf1efe9f0074e9692137cb
nyohoki <nyohoki@gmail.com>
2022-07-24 16:59:36 +0900

    gurasu

--- src/main/java/net/nyohoki/tsumiki/Tsumiki.java
+++ src/main/java/net/nyohoki/tsumiki/Tsumiki.java
@@ -1,7 +1,7 @@
 package net.nyohoki.tsumiki;
 
 import net.fabricmc.api.ModInitializer;
-import net.nyohoki.tsumiki.item.ModItems;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,11 +14,5 @@ public class Tsumiki implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		ModItems.registerModItems();
-
-		LOGGER.info("Hello Fabric world!");
 	}
 }
