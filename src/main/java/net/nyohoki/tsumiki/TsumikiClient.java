e330ed3eadfa07f3d286305e1067c94e51b6c8c5
nyohoki <nyohoki@gmail.com>
2022-07-24 14:54:50 +0900

    Initial commit

---
+++ src/main/java/net/nyohoki/tsumiki/TsumikiClient.java
@@ -0,0 +1,10 @@
+package net.nyohoki.tsumiki;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TsumikiClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
