373701fc7cf46fa6c5828b03e2ef0568a34dafe8
nyohoki <nyohoki@gmail.com>
2022-07-24 17:28:23 +0900

    testing1

--- src/main/java/net/nyohoki/tsumiki/Tsumiki.java
+++ src/main/java/net/nyohoki/tsumiki/Tsumiki.java
@@ -1,18 +1,20 @@
 package net.nyohoki.tsumiki;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.util.registry.Registry;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Tsumiki implements ModInitializer {
 	public static final String MOD_ID = "tsumiki";
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Block GURASU = new Block(FabricBlockSettings.of(Material.GLASS).strength(1.0f));
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Hello Fren :3");
 	}
 }
