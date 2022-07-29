18c517111b4c95b40a4fefbf776e70cc18d919ef
nyohoki <nyohoki@gmail.com>
2022-07-29 14:09:30 -0700

    Update Tsumiki.java

--- src/main/java/net/nyohoki/tsumiki/Tsumiki.java
+++ src/main/java/net/nyohoki/tsumiki/Tsumiki.java
@@ -53,47 +53,38 @@ public class Tsumiki implements ModInitializer {
 		LOGGER.info("Henlo Fren :3c");
 		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "gurasu"), GURASU);
 		Registry.register(Registry.ITEM, new Identifier("tsumiki", "gurasu"),
-				new BlockItem(GURASU, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
-		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_stairs"), GLASS_STAIRS);
-		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_stairs"),
-				new BlockItem(GLASS_STAIRS, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
-		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_slab"), GLASS_SLAB);
-		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_slab"),
-				new BlockItem(GLASS_SLAB, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
-		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_fence"), GLASS_FENCE);
-		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_fence"),
-				new BlockItem(GLASS_FENCE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
-		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_gate"), GLASS_GATE);
-		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_gate"),
-				new BlockItem(GLASS_GATE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
-		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_wall"), GLASS_WALL);
-		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_wall"),
-				new BlockItem(GLASS_WALL, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
-		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_trapdoor"), GLASS_TRAPDOOR);
-		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_trapdoor"),
-				new BlockItem(GLASS_TRAPDOOR, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
+			new BlockItem(GURASU, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		// glass
+		// clear
 		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_button"), GLASS_BUTTON);
 		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_button"),
-				new BlockItem(GLASS_BUTTON, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
+			new BlockItem(GLASS_BUTTON, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_carpet"), GLASS_CARPET);
 		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_carpet"),
-				new BlockItem(GLASS_CARPET, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
+			new BlockItem(GLASS_CARPET, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_door"), GLASS_DOOR);
 		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_door"),
-				new BlockItem(GLASS_DOOR, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
+			new BlockItem(GLASS_DOOR, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_fence"), GLASS_FENCE);
+		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_fence"),
+			new BlockItem(GLASS_FENCE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_gate"), GLASS_GATE);
+		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_gate"),
+			new BlockItem(GLASS_GATE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_pressure_plate"), GLASS_PRESSURE_PLATE);
 		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_pressure_plate"),
-				new BlockItem(GLASS_PRESSURE_PLATE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-
+			new BlockItem(GLASS_PRESSURE_PLATE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_slab"), GLASS_SLAB);
+		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_slab"),
+			new BlockItem(GLASS_SLAB, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_stairs"), GLASS_STAIRS);
+		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_stairs"),
+			new BlockItem(GLASS_STAIRS, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_trapdoor"), GLASS_TRAPDOOR);
+		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_trapdoor"),
+			new BlockItem(GLASS_TRAPDOOR, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.BLOCK, new Identifier("tsumiki", "glass_wall"), GLASS_WALL);
+		Registry.register(Registry.ITEM, new Identifier("tsumiki", "glass_wall"),
+			new BlockItem(GLASS_WALL, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
