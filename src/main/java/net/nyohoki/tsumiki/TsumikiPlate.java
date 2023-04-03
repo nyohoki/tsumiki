701e762aeadd90ca3b5a63c885fe1b8611209e69
nyohoki <nyohoki@gmail.com>
2023-04-03 15:25:51 -0700

    Update TsumikiPlate.java

--- src/main/java/net/nyohoki/tsumiki/TsumikiPlate.java
+++ src/main/java/net/nyohoki/tsumiki/TsumikiPlate.java
@@ -5,8 +5,8 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.PressurePlateBlock;
 
 public class TsumikiPlate extends PressurePlateBlock {
-    public TsumikiPlate(ActivationRule type, Settings settings) {
-        super(type, settings);
-    }
+	public TsumikiPlate(ActivationRule type, Settings settings) {
+		super(type, settings);
+	}
 
 }
