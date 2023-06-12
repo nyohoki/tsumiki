3ef1a1b646599ad7abfe9d38d0cd84a3f8e1e130
nyohoki <nyohoki@gmail.com>
2023-06-12 07:58:34 -0700

    Update and rename src/main/java/net/nyohoki/tsumiki/TsumikiStairs.java to src/main/java/nyohoki/tsumiki/TsumikiStairs.java

--- src/main/java/nyohoki/tsumiki/TsumikiStairs.java
+++ src/main/java/nyohoki/tsumiki/TsumikiStairs.java
@@ -4,8 +4,13 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
 
 public class TsumikiStairs extends StairsBlock {
-    public TsumikiStairs(BlockState baseBlockState,
-                         Settings settings) {
-        super(baseBlockState, settings);
-    }
+	public TsumikiStairs(
+		BlockState baseBlockState,
+		Settings settings
+	) {
+		super(
+			baseBlockState,
+			settings
+		);
+	}
 }
