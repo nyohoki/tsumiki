5a88802af32163314718508196a452fd0f530d09
nyohoki <nyohoki@gmail.com>
2023-06-15 08:10:32 -0700

    Create StairBlocks.java

---
+++ src/main/java/nyohoki/tsumiki/block/StairBlocks.java
@@ -0,0 +1,36 @@
+package nyohoki.tsumiki.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import nyohoki.tsumiki.Tsumiki;
+import nyohoki.tsumiki.TsumikiStairs;
+
+public class StairBlocks {
+	public static final Block GLASS_STAIRS = registerBlock("glass_stairs",
+		new TsumikiStairs(NyohokiBlock.NYOHOKI.getDefaultState(),
+			FabricBlockSettings.create()
+				.mapColor(MapColor.CLEAR)
+				.strength(0.3f, 0.3f)
+				.sounds(BlockSoundGroup.GLASS)
+				.nonOpaque()
+				)
+	);
+	private static Block registerBlock(String name, Block block) {
+		registerBlockItem(name, block);
+		return Registry.register(Registries.BLOCK, new Identifier(Tsumiki.MOD_ID, name), block);
+	}
+	private static Item registerBlockItem(String name, Block block) {
+		Item item = Registry.register(Registries.ITEM, new Identifier(Tsumiki.MOD_ID, name),
+			new BlockItem(block, new FabricItemSettings()));
+	}
+	public static void registerStairBlocks() {
+		Tsumiki.LOGGER.info("Registering Stair Blocks for " + Tsumiki.MOD_ID);
+	}
+}
